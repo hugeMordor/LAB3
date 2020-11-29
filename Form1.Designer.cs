5ed3244e25abb248c119482bd46d85328cc3be61
hugeMordor <72040616+hugeMordor@users.noreply.github.com>
2020-11-29 17:03:38 +0300

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -47,11 +47,13 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(591, 12);
+            this.button1.Location = new System.Drawing.Point(628, 197);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -61,21 +63,21 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(591, 60);
+            this.textBox1.Location = new System.Drawing.Point(617, 41);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(591, 86);
+            this.textBox2.Location = new System.Drawing.Point(617, 67);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 3;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(591, 112);
+            this.textBox3.Location = new System.Drawing.Point(617, 93);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 20);
             this.textBox3.TabIndex = 4;
@@ -83,7 +85,7 @@
             // textBox4
             // 
             this.textBox4.Enabled = false;
-            this.textBox4.Location = new System.Drawing.Point(591, 138);
+            this.textBox4.Location = new System.Drawing.Point(617, 119);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(100, 20);
             this.textBox4.TabIndex = 5;
@@ -91,7 +93,7 @@
             // textBox5
             // 
             this.textBox5.Enabled = false;
-            this.textBox5.Location = new System.Drawing.Point(591, 164);
+            this.textBox5.Location = new System.Drawing.Point(617, 145);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(100, 20);
             this.textBox5.TabIndex = 6;
@@ -99,52 +101,52 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(698, 66);
+            this.label1.Location = new System.Drawing.Point(724, 47);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.Size = new System.Drawing.Size(112, 13);
             this.label1.TabIndex = 7;
-            this.label1.Text = "V0";
+            this.label1.Text = "Начальная скорость";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(697, 89);
+            this.label2.Location = new System.Drawing.Point(723, 70);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.Size = new System.Drawing.Size(32, 13);
             this.label2.TabIndex = 8;
-            this.label2.Text = "alpha";
+            this.label2.Text = "Угол";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(697, 115);
+            this.label3.Location = new System.Drawing.Point(723, 96);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.Size = new System.Drawing.Size(102, 13);
             this.label3.TabIndex = 9;
-            this.label3.Text = "h0";
+            this.label3.Text = "Начальная высота";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(697, 141);
+            this.label4.Location = new System.Drawing.Point(723, 122);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.Size = new System.Drawing.Size(77, 13);
             this.label4.TabIndex = 10;
-            this.label4.Text = "Hmax";
+            this.label4.Text = "Макс. высота";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(697, 167);
+            this.label5.Location = new System.Drawing.Point(723, 148);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.Size = new System.Drawing.Size(99, 13);
             this.label5.TabIndex = 11;
-            this.label5.Text = "Lmax";
+            this.label5.Text = "Макс. расстояние";
             // 
             // textBox6
             // 
             this.textBox6.Enabled = false;
-            this.textBox6.Location = new System.Drawing.Point(591, 190);
+            this.textBox6.Location = new System.Drawing.Point(617, 171);
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(100, 20);
             this.textBox6.TabIndex = 12;
@@ -152,19 +154,19 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(698, 193);
+            this.label6.Location = new System.Drawing.Point(724, 174);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(33, 13);
+            this.label6.Size = new System.Drawing.Size(78, 13);
             this.label6.TabIndex = 13;
-            this.label6.Text = "Tmax";
+            this.label6.Text = "Время полета";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(686, 12);
+            this.button2.Location = new System.Drawing.Point(617, 226);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(113, 23);
             this.button2.TabIndex = 14;
-            this.button2.Text = "Старт";
+            this.button2.Text = "Старт/Продолжить";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.Button2_Click);
             // 
@@ -175,7 +177,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(591, 234);
+            this.button3.Location = new System.Drawing.Point(628, 12);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 15;
@@ -185,7 +187,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(767, 12);
+            this.button4.Location = new System.Drawing.Point(736, 226);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 16;
@@ -195,7 +197,7 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(767, 42);
+            this.button5.Location = new System.Drawing.Point(617, 255);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 23);
             this.button5.TabIndex = 17;
@@ -203,11 +205,33 @@
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.Button5_Click);
             // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(736, 255);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 18;
+            this.button6.Text = "Очистка";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(725, 12);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 19;
+            this.button7.Text = "Сетка";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button7_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(844, 637);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -252,6 +276,8 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,11 +16,9 @@ namespace LAB
             InitializeComponent();
         }
         public double V0, V0x, V0y, Hmax, Lmax, Tmax, h0, alpha, t, n, n0x, n0y;
-
         private void Button2_Click(object sender, EventArgs e)
         {
             timer1.Enabled = true;
-
         }
 
         private void Timer1_Tick(object sender, EventArgs e)
@@ -31,28 +29,38 @@ namespace LAB
             y = Math.Round(h0 + (n0y * t) - (9.81 * t * t) / 2);
         }
 
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            fullW = Form1.ActiveForm.ClientSize.Width;
+            fullH = Form1.ActiveForm.ClientSize.Height;
+            Graphics spot3 = Form1.ActiveForm.CreateGraphics();
+            Brush Pen3 = new SolidBrush(Color.White);
+            spot3.FillRectangle(Pen3, 0, 0, fullW, fullH);
+            x0 = 0;y0 = 0;
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            Table();
+        }
+
         private void Button3_Click(object sender, EventArgs e)
         {
             Axis();
         }
-
         private void Button4_Click(object sender, EventArgs e)
         {
             timer1.Enabled = false;
         }
-
         private void Button5_Click(object sender, EventArgs e)
         {
             timer1.Enabled = false;
             x = 0; y = 0; t = 0;
-
         }
-
         public void Input()
         {
             try
             {
-
                 V0 = Convert.ToDouble(textBox1.Text);
                 alpha = Convert.ToDouble(textBox2.Text);
                 h0 = Convert.ToDouble(textBox3.Text);
@@ -72,12 +80,10 @@ namespace LAB
         }
         public void Calc()
         {
-
             V0x = V0 * Math.Cos(alpha * Math.PI / 180);
             V0y = V0 * Math.Sin(alpha * Math.PI / 180);
             n0x = n * Math.Cos(alpha * Math.PI / 180);
             n0y = n * Math.Sin(alpha * Math.PI / 180);
-
             Hmax = h0 + (V0y * V0y) / (2 * 9.81);
             Tmax = (V0y + Math.Sqrt(V0y * V0y + 2 * 9.81 * h0)) / 9.81;
             Lmax = V0x * Tmax;
@@ -94,33 +100,20 @@ namespace LAB
             Calc();
             Output();
         }
-
-
-        
-        
             public int W, H, fullW, fullH;
             public double x, y;
-
             public void Axis()
             {
-            
-               
                 Graphics g = Form1.ActiveForm.CreateGraphics();
-             
                 Pen Pen;
-                Pen = new Pen(Color.Violet);
+                Pen = new Pen(Color.Green);
                 Pen.Width = 5;
-               
-                
                 fullW = Form1.ActiveForm.ClientSize.Width; 
                 fullH = Form1.ActiveForm.ClientSize.Height; 
-          
-            
-            Point KX1, KX2;
+                Point KX1, KX2;
                 KX1 = new Point(0, fullH);
                 KX2 = new Point(fullW, fullH);
                 g.DrawLine(Pen, KX1, KX2);
-          
                 Point KY1, KY2;
                 KY1 = new Point(0, 0);
                 KY2 = new Point(0, fullH);
@@ -130,10 +123,36 @@ namespace LAB
         public void Graph()
         {
             Graphics spot = Form1.ActiveForm.CreateGraphics();
-            Pen Pen2; Pen2 = new Pen(Color.Black);
+            Pen Pen2; Pen2 = new Pen(Color.Red);
             Pen2.Width = 2;
-            
             spot.DrawLine(Pen2, Convert.ToInt32(x - 1), Convert.ToInt32(fullH-y - 1), Convert.ToInt32(x + 1), Convert.ToInt32(fullH-y + 1));
         }
-    }
+        public int x0, y0;
+        public void Table()
+        {
+            Graphics tab = Form1.ActiveForm.CreateGraphics();
+            Pen Pent;
+            Pent = new Pen(Color.Black);
+            Pent.Width = 0.2F;
+            fullW = Form1.ActiveForm.ClientSize.Width;
+            fullH = Form1.ActiveForm.ClientSize.Height;
+            while (x0 < fullW)
+            {
+                Point TX1, TX2;
+                TX1 = new Point(x0 + 100, fullH);
+                TX2 = new Point(x0 + 100, 0);
+                tab.DrawLine(Pent, TX1, TX2);
+                x0 += 100;
+            }
+            while (y0 < fullH)
+            {
+                Point TY1, TY2;
+                TY1 = new Point(0, fullH-y0 + 100);
+                TY2 = new Point(fullW, fullH-y0 + 100);
+                tab.DrawLine(Pent, TY1, TY2);
+                y0 += 100;
+            }
+            tab.Dispose();
+        }
+    }   
 }
