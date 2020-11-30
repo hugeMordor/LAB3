916c101e6992ff9134790bc4d99692474903b610
hugeMordor <72040616+hugeMordor@users.noreply.github.com>
2020-11-30 15:30:45 +0300

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -112,9 +112,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(723, 70);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.Size = new System.Drawing.Size(89, 13);
             this.label2.TabIndex = 8;
-            this.label2.Text = "Угол";
+            this.label2.Text = "Угол в градусах";
             // 
             // label3
             // 
